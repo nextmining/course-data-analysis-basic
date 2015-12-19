ba0ef853482034dbee7cc1cc021b4418a870599e
louie <louiezzang@gmail.com>
2015-12-19 21:57:21 +0900

    Update R scripts

--- r-scripts/06_logistic_regression.R
+++ r-scripts/06_logistic_regression.R
@@ -1,13 +1,38 @@
-### 다항 로지스틱 회귀분석(Multinomial Logistic Regression)
+### 로지스틱 회귀분석(Logistic Regression)
 # http://rstudio-pubs-static.s3.amazonaws.com/14459_af8466cb23844215b74c03a471bf8e3c.html
 # 예측하고자하는 분류가 0, 1의 두개가 아닌, 여러개가 될수 있는 경우 (0, 1, 2같은) 
 # Multinomial Logistic Regression을 사용한다.
 
 library(nnet)
-iris
-m <- multinom(Species ~ ., data = iris)
 
+# Sepal Length, Width: 꽃받침의 길이, 두께
+# Petal Length, Width: 꽃잎의 길이, 두께
+# Species: Iris 붓꽃의 종류
+#   setosa
+#   virginica
+#   versicolor
+names(iris)
+iris
+
+?multinom
+m <- multinom(Species ~ ., data = iris)
 m
 
-# 작성한 모델이 주어진 훈련데이터를 어떻게 분류하고 있는지는 fitted()를 사용해 구할 수 있다.
-head(fitted(m))
+## 작성한 모델이 주어진 훈련데이터를 어떻게 분류하고 있는지는 fitted()를 사용해 구할 수 있다.
+#head(fitted(m))
+fitted(m)
+
+a <- apply(fitted(m), 1, max)
+ifelse(a == 1, "setosa", ifelse(a == 2, "versicolor", "virginica"))
+predict(m)
+
+## 테스트
+predict(m, newdata = iris[c(1, 51, 101), ], type = "class")
+predict(m, newdata = iris[c(1, 51, 101), ], type = "probs")
+
+## 모델 평가:
+# 모델의 정확도를 확인하기 위해 예측된 Species와 실제 Species를 비교한다.
+predicted <- predict(m, newdata = iris)
+sum(predicted == iris$Species)/NROW(iris)
+# 분할표로 확인
+xtabs(~predicted + iris$Species)
--- r-scripts/07_kmeans_clustering.R
+++ r-scripts/07_kmeans_clustering.R
@@ -1,15 +1,24 @@
-##
+### k-means clustering
 # http://rischanlab.github.io/Kmeans.html
 # http://www.rdatamining.com/examples/kmeans-clustering
 
+# Sepal Length, Width: 꽃받침의 길이, 두께
+# Petal Length, Width: 꽃잎의 길이, 두께
+# Species: Iris 붓꽃의 종류
+#   setosa
+#   virginica
+#   versicolor
 names(iris)
+iris
 
 # 최종적으로 분류할 라벨(Species)은 y변수에 할당하고 
 # 설명변수 x에는 라벨(Species) 필드는 제외한다.
 x <- iris[,-5]
 y <- iris$Species
 
-kc <- kmeans(x,3)
+?kmeans
+# number of clusters = 3
+kc <- kmeans(x, 3)
 kc
 
 # 클러스터링된 결과를 실제 분류라벨(y)과 비교해 보자.
@@ -17,3 +26,6 @@ table(y, kc$cluster)
 
 plot(x[c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
 points(kc$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 23, cex = 3)
+
+plot(x[c("Petal.Length", "Petal.Width")], col = kc$cluster)
+points(kc$centers[,c("Petal.Length", "Petal.Width")], col = 1:3, pch = 23, cex = 3)
